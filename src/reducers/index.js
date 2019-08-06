f23abcc3dc3429d76221f1da0e4232e533b10514
Alexey Bahter <meyson.24@yandex.ru>
2019-08-06 12:25:55 +0300

    .gitignore fix

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { bookReducer } from './book'
-import { userReducer } from './user'
-
-export const rootReducer = combineReducers({
-    book: bookReducer,
-    user: userReducer,
-});
